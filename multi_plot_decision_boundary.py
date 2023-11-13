c7b2b96532ed495b31783b46ab6af70dec687346
Jonathan Barrios <git@jonathanbarrios.com>
2023-11-13 11:53:37 -0600

    Update multi_plot_decision_boundary.py

--- multi_plot_decision_boundary.py
+++ multi_plot_decision_boundary.py
@@ -1,4 +1,4 @@
-from matplotlib.lines import Line2D  # Add this line
+from matplotlib.lines import Line2D
 def plot_decision_boundary(model, X, y):
     x_min, x_max = X[:, 0].min() - 0.1, X[:, 0].max() + 0.1
     y_min, y_max = X[:, 1].min() - 0.1, X[:, 1].max() + 0.1
@@ -15,21 +15,25 @@ def plot_decision_boundary(model, X, y):
 
     # Define class labels and colors
     class_labels = ["Mammal", "Bird", "Reptile"]
-    class_colors = ['blue', 'green', 'red']
+    class_colors = ['yellow', 'blue', 'red']
 
-    # Create a legend with class labels and colors
+    # Create a legend with class labels and matching class colors
     legend_elements = [Line2D([0], [0], marker='o', color='w', label=label, markerfacecolor=color, markersize=10)
                        for label, color in zip(class_labels, class_colors)]
 
     plt.contourf(XX, YY, Z.reshape(XX.shape), levels=np.arange(Z.max() + 2) - 0.5, cmap='RdBu', alpha=0.5)
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdBu, lw=0)
+    
+    # Assign colors to each class
+    scatter_colors = [class_colors[label] for label in y]
+
+    plt.scatter(X[:, 0], X[:, 1], color=scatter_colors, lw=0)
     plt.xlim(XX.min(), XX.max())
     plt.ylim(YY.min(), YY.max())
 
     # Set the background color to green
     plt.gca().set_facecolor('green')
 
-    # Add the legend to the top left corner
-    plt.legend(handles=legend_elements, loc='upper left')
+    # Add the legend to the top left corner outside the plot
+    plt.legend(handles=legend_elements, loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.show()
